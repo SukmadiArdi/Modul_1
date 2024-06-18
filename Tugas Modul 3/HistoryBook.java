1e419f5bc63bb1a689d11790d1d936f4a8318372
Achmad Ardi Sukmadi <145636141+SukmadiArdi@users.noreply.github.com>
2024-06-18 19:56:10 +0700

    Delete Tugas Modul 3 directory

--- Tugas Modul 3/HistoryBook.java
+++
@@ -1,8 +0,0 @@
-public class HistoryBook extends Book{
-    private String category;
-
-    public HistoryBook(String bookId, String title, String author, String category,int stock){
-        super(bookId, title, author,category, stock);
-
-    }
-}
